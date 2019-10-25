3741029c1525c7360d870c3d2611e199ae210531
iwannafly90 <dima3429@mail.ru>
2019-10-26 00:20:10 +0300

    Returned SQLALCHEMY_DATABASE_URI statement

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-import os
-
 
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('sqlalchemy_uri')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:{NewPassword}@localhost/flask_blog'
