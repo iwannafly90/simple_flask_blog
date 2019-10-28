b67f7fa72663bee2b9dd36e0099867d14e46fe27
iwannafly90 <dima3429@mail.ru>
2019-10-28 22:27:22 +0300

    Final commit

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:{NewPassword}@localhost/flask_blog'
+    SECRET_KEY = 'something very secret'
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
--- models.py
+++ models.py
@@ -3,6 +3,8 @@ import re
 from app import db
 from datetime import datetime
 
+from flask_security import UserMixin, RoleMixin
+
 
 def slugify(s):
     pattern = r'[^\w+]'
@@ -27,7 +29,7 @@ class Post(db.Model):
         self.generate_slug()
 
     tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
-    
+
     def generate_slug(self):
         if self.title:
             self.slug = slugify(self.title)
@@ -47,3 +49,23 @@ class Tag(db.Model):
 
     def __repr__(self):
         return f'Tag id: {self.id}, name: {self.name}'
+
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+                       )
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
